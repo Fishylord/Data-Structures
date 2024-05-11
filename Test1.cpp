bfefc689079f33226b268043b20c5729d769ef68
Fishylord <chunyee04@outlook.com>
2024-05-11 15:33:17 +0800

    as

--- Test1.cpp
+++ Test1.cpp
@@ -1,104 +1,188 @@
 #include <iostream>
-#include <vector>
-using namespace std;
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <algorithm>
+#include <random>
+#include <ctime>
 
+struct Question {
+    std::string id;
+    std::string question;
+    std::string choices;
+    std::string answer;
+    int marks;
+};
+
+struct Node {
+    Question data;
+    Node* next;
+};
+
+class LinkedList {
+public:
+    Node* head;
+    LinkedList() : head(nullptr) {}
+
+    ~LinkedList() {
+        Node* current = head;
+        while (current != nullptr) {
+            Node* next = current->next;
+            delete current;
+            current = next;
+        }
+        head = nullptr;
+    }
+
+    void addNode(Question q) {
+        Node* newNode = new Node{ q, nullptr };
+        if (head == nullptr) {
+            head = newNode;
+        }
+        else {
+            Node* temp = head;
+            while (temp->next != nullptr) {
+                temp = temp->next;
+            }
+            temp->next = newNode;
+        }
+    }
+
+    Question removeFront() {
+        if (head == nullptr) {
+            throw std::runtime_error("List is empty");
+        }
+        Node* temp = head;
+        Question q = temp->data;
+        head = head->next;
+        delete temp;
+        return q;
+    }
+
+    void displayFirst() {
+       Node* temp = head;
+       std::cout << "ID: " << temp->data.id << "\nQuestion: " << temp->data.question
+           << "\nChoices: " << temp->data.choices << "\nAnswer: " << temp->data.answer
+           << "\nMarks: " << temp->data.marks << "\n\n";
+    }
+
+    void displayLast() {
+       Node* temp = head;
+       while (temp->next != nullptr) {
+           temp = temp->next;
+       }
+       std::cout << "ID: " << temp->data.id << "\nQuestion: " << temp->data.question
+           << "\nChoices: " << temp->data.choices << "\nAnswer: " << temp->data.answer
+           << "\nMarks: " << temp->data.marks << "\n\n";
+    }
+
+    void display() {
+        Node* temp = head;
+        while (temp != nullptr) {
+            std::cout << "ID: " << temp->data.id << "\nQuestion: " << temp->data.question
+                << "\nChoices: " << temp->data.choices << "\nAnswer: " << temp->data.answer
+                << "\nMarks: " << temp->data.marks << "\n\n";
+            temp = temp->next;
+        }
+    }
+};
+
+void shuffleQuestions(LinkedList& list) {
+    std::vector<Question> questions;
+    Node* current = list.head;
+    while (current != nullptr) {
+        questions.push_back(current->data);
+        current = current->next;
+    }
+
+    std::random_device rd;
+    std::mt19937 g(rd());
+    std::shuffle(questions.begin(), questions.end(), g);
+
+    current = list.head;
+    for (auto& q : questions) {
+        current->data = q;
+        current = current->next;
+    }
+}
 
 int main() {
-    // int lucky_number = 10;
-    // std::cout << "Hello, world!";
-    // return 0;
+    LinkedList unansweredDeck, answeredDeck, discardPile;
+    std::string filename = "question.txt";
+    std::ifstream file(filename);
 
-    // int a = 0;
-    // while (a<5) {
-    //     cout << a << endl;
-    //     a++;
-    // }
-    // return 0;
-
-    // std::cout << "Please enter value";
-    // int number;
-    // std::cin >> number;
-    // std::cout << "you entered:" << number << std::endl;
-    // return 0;
-
-    // std::vector<int> numbers = {10,15,20};
-    // std::cout << "List of numbers:\n";
-    // for (int num : numbers){
-    //     std::cout <<num << std::endl;
-    // }
-
-    // std::cout << "pleast pick a number (1,2 or 3):";
-
-    // int choice;
-    // std::cin >> choice;
-
-    // switch(choice){
-    //     case 1:
-    //         std::cout <<"you picked " <<numbers[0] <<".\n";
-    //         break;
-    //     case 2:
-    //         std::cout << "you picked " <<numbers[1] <<".\n";
-    //         break;
-    //     case 3:
-    //         std::cout << "you picked " <<numbers[2] <<".\n";
-    //         break;
-    //     default:
-    //         std::cout << "Invalid choice. Please pick 1, 2, or 3. \n";
-    //         break;
-    // }    
-    // return 0;
-    string Card1 = "Yo mama";
-    string Card2 = "Amongussy";
-    string Card3 = "Sussy";
-    string Ucard1 = "baka";
-    string Ucard2 = "Fishy";
-    string Ucard3 = "whatever";
-    string Ans1 = "yes";
-    string Ans2 = "yes";
-    string Ans3 = "no";
-
-    std::vector<string> Choices ={"Discarded Pile", "Unanswered Pile"};
-    //card Pile
-    std::vector<string> Dcard ={Card1,Card2,Card3};
-    std::vector<string> Uncard ={Ucard1,Ucard2,Ucard3};
-    std::vector<string> DPile ={Ans1,Ans2,Ans3};
-    std::cout << "please pick a choice Discarded or Unanswered.\n";
-
-
-    string Choices1,UChoices;
-    int DChoices;
-    string DAnswer,UAnswer;
-    std::cin >> Choices1;
-    std::cout << Choices1;
-    if(Choices1 == "Discarded"){
-        std::cout << "These are the items:\n";
-        for (const std::string& choice : Dcard) {//string& makes a reference that directly effects the orginal variable and const ensures it doesn't get changed.
-            std::cout << choice << std::endl;
-        }
-
-        std::cout << "which one? :";
-        std::cin >> DChoices;
-        DChoices --;
-
-        if (DChoices >= 0 && DChoices < Dcard.size()) {
-            std::cout << "Answer: ";
-            std::cin >> DAnswer;
-            // Answer is stored in order of the card so picking 1 gives first answer.
-            if (DAnswer == DPile[DChoices]) {
-                std::cout << "Correct Answer." <<std ::endl;
-            } else {
-                std::cout << "Incorrect answer." << std::endl;
-            }
-        } else {
-            std::cout << "Invalid choice." << std::endl;
-        }
-
-
-    } else if (Choices1 == "Unanswered") { // Needs to be updated.
-        std::cout << "This is your card:\n";
-        
-    } else {
-        std::cout<< "type better stupid";
+    if (!file.is_open()) {
+        std::cerr << "Error opening file: " << filename << std::endl;
+        return 1;
     }
+
+    std::string line;
+    while (getline(file, line)) {
+        std::istringstream ss(line);
+        Question q;
+        getline(ss, q.id, ',');
+        getline(ss, q.question, ',');
+        getline(ss, q.choices, ',');
+        getline(ss, q.answer, ',');
+        ss >> q.marks;
+        unansweredDeck.addNode(q);
+    }
+
+    file.close();
+    shuffleQuestions(unansweredDeck);
+
+    // User interaction
+    char response;
+    while (unansweredDeck.head != nullptr) {
+        unansweredDeck.displayFirst();  // Show only the current question
+        std::cout << "Do you want to answer this question? (y/n): ";
+        std::cin >> response;
+
+        if (response == 'y') {
+            answeredDeck.addNode(unansweredDeck.removeFront());
+        }
+        else {
+            discardPile.addNode(unansweredDeck.removeFront());
+        }
+    }
+
+    char view;
+    view = '9';
+    while (view != '0') {
+        std::cout << "Select what to do:\n1)View all Answered Questions\n2)View all Discarded Questions\n3)Most Recent Answered Card\n4)Most Recent Discarded Card\n5)Earliest Answered Card\n6)Earliest Discarded Card\n0) Exit\n";
+        std::cin >> view;
+        
+        if (view == '1') {
+            std::cout << "\nAnswered Questions:\n";
+            answeredDeck.display();
+        }
+        else if (view == '2') {
+            std::cout << "\nDiscarded Questions:\n";
+            discardPile.display();
+        }
+        else if (view == '3') {
+            std::cout << "\nMost Recent Answered Card:\n";
+            answeredDeck.displayLast();
+        }
+        else if (view == '4') {
+            std::cout << "\nMost Recent Discarded Card:\n";
+            discardPile.displayLast();
+        }
+        else if (view == '5') {
+            std::cout << "\nEarliest Answered Card:\n";
+            answeredDeck.displayLast();
+        }
+        else if (view == '6') {
+            std::cout << "\nEarliest Discarded Card:\n";
+            discardPile.displayLast();
+        }
+        else if (view == '0') {
+            break;
+        }
+        else {
+            std::cout << "\nNot a valid Choice\n\n";
+        }
+    }
+
     return 0;
 }
